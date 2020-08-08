d3e752762bcd5855d556e72180251d6aede7bc07
Stephen Junge <steve.junge@gmail.com>
2020-08-08 23:30:06 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,7 +66,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
       holidays.each do |holiday, supplies|
-        puts holiday.to_s.capitalize + ":" + supplies
+        puts holiday.to_s.capitalize + ":" 
+        puts supplies.to_s.capitalize
       
     end
   end
