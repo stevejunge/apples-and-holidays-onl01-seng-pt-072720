2afe56bb2d30fbd86160c6915c22ae59c7fd5304
Stephen Junge <steve.junge@gmail.com>
2020-08-08 23:33:24 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,7 +61,14 @@ def all_holidays_with_bbq(holiday_hash)
   end.flatten.compact
 end
 
-all_holidays_with_bbq(holiday_supplies)
+def all_supplies_in_holidays(holiday_hash)
+  holiday_hash.each do |season, holiday|
+    puts "#{season.capitalize}:"
+    holiday.each do |holiday, supplies|
+    puts "  #{holiday.to_s.split("_").map{|x| x.capitalize}.join(" ")}: #{supplies.join(", ")}"
+  end
+end
+end
 
 
 
